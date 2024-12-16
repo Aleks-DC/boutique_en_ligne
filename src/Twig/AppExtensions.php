41e75bbb9eb80820863a559664a1beeefc70cc04
Aleks_DC <165147128+Aleks-DC@users.noreply.github.com>
2024-12-16 16:53:35 +0100

    AppExtension Price + ProductController + Product Twig View

---
+++ src/Twig/AppExtensions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtensions extends AbstractExtension
+{
+	public function getFilters()
+	{
+		return [
+			new TwigFilter('price', [$this, 'formatPrice']),
+		];
+	}
+
+	public function formatPrice($number, $decimals = 0, $decPoint = '.', $thousandsSep = ',')
+	{
+		return number_format($number, '2', ','). ' €';
+
+	}
+}
