177c7d9fc54a62eb4f28b0a78e7ab562d209647a
Aleks_DC <165147128+Aleks-DC@users.noreply.github.com>
2024-12-17 21:18:05 +0100

    AppExtensions getGlobals + base.html.twig loop on categories

--- src/Twig/AppExtensions.php
+++ src/Twig/AppExtensions.php
@@ -2,21 +2,35 @@
 
 namespace App\Twig;
 
+use App\Repository\CategoryRepository;
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtensions extends AbstractExtension
+class AppExtensions extends AbstractExtension implements GlobalsInterface
 {
-	public function getFilters()
+	private CategoryRepository $categoryRepository;
+	public function __construct(CategoryRepository $categoryRepository)
+	{
+		$this->categoryRepository = $categoryRepository;
+	}
+	public function getFilters(): array
 	{
 		return [
-			new TwigFilter('price', [$this, 'formatPrice']),
+			new TwigFilter('price', $this->formatPrice(...)),
 		];
 	}
 
-	public function formatPrice($number, $decimals = 0, $decPoint = '.', $thousandsSep = ',')
+	public function formatPrice($number, $decimals = 0, $decPoint = '.', $thousandsSep = ','): string
 	{
 		return number_format($number, '2', ','). ' €';
 
+	}
+
+	public function getGlobals(): array
+	{
+		return [
+			'allCategories' => $this->categoryRepository->findAll(),
+		];
 	}
 }
